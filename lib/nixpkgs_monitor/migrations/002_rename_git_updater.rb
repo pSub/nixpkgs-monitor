401647821df27ad7f350b487603ca18aef4ff34c
Pascal Wittmann <mail@pascal-wittmann.de>
2016-11-13 17:34:39 +0100

    Fix migrations

--- lib/nixpkgs_monitor/migrations/002_rename_git_updater.rb
+++ lib/nixpkgs_monitor/migrations/002_rename_git_updater.rb
@@ -2,9 +2,9 @@ Sequel.migration do
   change do
 
     # rename tables to match updater renames
-    DB.rename_table(:repository_fetchgit, :git_fetchgit)
-    DB.rename_table(:repository_github, :git_github)
-    DB.rename_table(:repository_metagit, :git_metagit)
+    rename_table(:repository_fetchgit, :git_fetchgit)
+    rename_table(:repository_github, :git_github)
+    rename_table(:repository_metagit, :git_metagit)
 
   end
 end
